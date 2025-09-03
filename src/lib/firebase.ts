9826a8c4b9e065e1b6451a73b7d7bae00eb785ab
Goody Devs <devswithgoody82@gmail.com>
2025-09-03 11:41:22 +0000

    Use .env for firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: 'taskflow-zhpsl',
-  appId: '1:383473825701:web:21e2462da982068be7da4c',
-  storageBucket: 'taskflow-zhpsl.firebasestorage.app',
-  apiKey: 'AIzaSyBX3rZq81bMvBodmaW-CwWHqyPC62omoMI',
-  authDomain: 'taskflow-zhpsl.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '383473825701',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
